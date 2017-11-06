4a24bee8098009e599332b585226abbfd5f58fbd
dougmcneall <dougmcneall@gmail.com>
2017-11-06 14:35:18 +0000

    added pairs plot example

--- exeter_meeting.R
+++ exeter_meeting.R
@@ -4,3 +4,35 @@
 
 
 
+# Example of adding  density plots to a pairs plot
+dfunc.up <- function(x,y,...){
+  require(MASS)
+  require(RColorBrewer)
+  
+  rb = brewer.pal(9, "RdBu")
+  br  = rev(rb)
+  # function for plotting 2d kernel density estimates in pairs() plot.
+  kde = kde2d(x,y)
+  image(kde, col = rb, add = TRUE)
+}
+
+rb = brewer.pal(9, "RdBu")
+br  = rev(rb)
+
+pdf(file = 'pairs_dens.pdf', width = 8, height = 8)
+par(oma = c(0,0,0,3))
+
+# Emulate all input space and keep only those inputs which match a 
+# criteria (such as having absolute error below a threshold)
+
+test = pairs(X.unif[ix.kept, ],
+             gap = 0, lower.panel = NULL, xlim = c(0,1), ylim = c(0,1),
+             panel = dfunc.up)
+
+image.plot(legend.only = TRUE,
+           zlim = c(0,1),
+           col = rb,
+           legend.args = list(text = 'Density of model runs matching the criteria', side = 3, line = 1),
+           horizontal = TRUE
+)
+dev.off()
